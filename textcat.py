f18957742d4b23163eb07476b45cc7fc9be780fb
dshenker <dshenke1@jhu.edu>
2021-10-03 17:59:06 -0400

    update

--- textcat.py
+++ textcat.py
@@ -59,7 +59,7 @@ def file_log_prob(file: Path, lm: LanguageModel) -> float:
     log_prob = 0.0
     for (x, y, z) in read_trigrams(file, lm.vocab):
         prob = lm.prob(x, y, z)  # p(z | xy)
-        log_prob += math.log(prob)
+        log_prob += math.log(prob) 
     return log_prob
 
 
@@ -71,7 +71,6 @@ def main():
     lm1 = LanguageModel.load(args.model1)
     lm2 = LanguageModel.load(args.model2)
     prior = args.prior
-    
 
     # We use natural log for our internal computations and that's
     # the kind of log-probability that file_log_prob returns.
