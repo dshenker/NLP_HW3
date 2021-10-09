3f6f8e8846401da01d408dbafa7a2ae5e00e019e
asomaya1 <asomaya1@jhu.edu>
2021-10-09 18:41:30 -0400

    got some improvements

--- probs.py
+++ probs.py
@@ -125,7 +125,7 @@ def draw_trigrams_forever(file: Path,
     else:
         import random
         pool = tuple(trigrams)
-        while batch_count < int(50/batch_size):
+        while batch_count < int(2/batch_size):
             #print("na")
             for trigram in random.sample(pool, len(pool)):
                 #print("Go")
@@ -590,7 +590,7 @@ class ImprovedLogLinearLanguageModel(EmbeddingLogLinearLanguageModel):
         # training, but those wouldn't use nn.Parameter.
         self.X = nn.Parameter(torch.zeros((self.dim, self.dim)), requires_grad=True)
         self.Y = nn.Parameter(torch.zeros((self.dim, self.dim)), requires_grad=True)
-        #self.XYZ = nn.Parameter(torch.zeros((self.dim, 1)), requires_grad=True)
+        self.XYZ = nn.Parameter(torch.zeros((self.dim, 1)), requires_grad=True)
         #self.x_OOV = nn.Parameter(torch.zeros((self.dim, 1)), requires_grad=True)
         #self.y_OOV = nn.Parameter(torch.zeros((self.dim, 1)), requires_grad=True)
 
@@ -624,38 +624,38 @@ class ImprovedLogLinearLanguageModel(EmbeddingLogLinearLanguageModel):
         if z in self.words:
             z_embed = self.embed_mat[self.words.index(z)]
         else:
-           # z_OOV = True
+            z_OOV = True
             z_embed = self.embed_mat[self.words.index('OOL')]
 
 
-        #xyz_features = y_embed*z_embed*x_embed
-        #xyz_features_denom = (y_embed*x_embed)*self.Z_mat
+        xyz_features = y_embed*z_embed*x_embed
+        xyz_features_denom = (y_embed*x_embed)*self.Z_mat
         #OOV_feature = torch.zeros((self.dim,1))
 
         #if z_OOV:
         #    OOV_feature = self.x_OOV*x_embed + self.y_OOV*y_embed
 
-        numerator = torch.t(x_embed)@self.X@z_embed + torch.t(y_embed)@self.Y@z_embed
         # + self.XYZ.T@xyz_features
-        denominator = torch.t(x_embed)@self.X@torch.t(self.Z_mat) + torch.t(y_embed)@self.Y@torch.t(self.Z_mat)
         # + (self.XYZ.T)@(xyz_features_denom.T)
-        denominator = torch.logsumexp(denominator, 0, keepdim = False)
-        
+
+        numerator = torch.t(x_embed)@self.X@z_embed + torch.t(y_embed)@self.Y@z_embed + self.XYZ.T@xyz_features
+        denominator = torch.t(x_embed)@self.X@torch.t(self.Z_mat) + torch.t(y_embed)@self.Y@torch.t(self.Z_mat) + (self.XYZ.T)@(xyz_features_denom.T)
+        denominator = torch.logsumexp(denominator.squeeze(), 0, keepdim = False)
         return (numerator - denominator)
 
     def train(self, file: Path):    # type: ignore
 
-        gamma0 = 0.01  # initial learning rate
+        gamma0 = 0.001  # initial learning rate
 
         # This is why we needed the nn.Parameter above.
         # The optimizer needs to know the list of parameters
         # it should be trying to update.
-        optimizer = optim.SGD(self.parameters(), lr=gamma0)
+        optimizer = optim.Adam(self.parameters(), lr=gamma0)
 
         # Initialize the parameter matrices to be full of zeros.
         nn.init.zeros_(self.X)   # type: ignore
         nn.init.zeros_(self.Y)   # type: ignore
-        #nn.init.zeros_(self.XYZ)
+        nn.init.zeros_(self.XYZ)
         #nn.init.zeros_(self.y_OOV)
         #nn.init.zeros_(self.x_OOV)
 
@@ -664,30 +664,25 @@ class ImprovedLogLinearLanguageModel(EmbeddingLogLinearLanguageModel):
         log.info("Start optimizing on {N} training tokens...")
 
         C = 1
-        epochs = 10
-        batch_size = 1
+        epochs = 50
+        batch_size = 20
+        epoch_loss = 0
 
-        #print("artafinde")
         for i in range(epochs):
-            #print("elephat")
-            batch = []
-            #print("potao")
+            epoch_loss = 0
             for batch in draw_trigrams_forever(file, self.vocab, randomize=True):
-                #print(batch)
                 F = 0
                 for trig in batch: #VECTORIZE COMPUTATION OVER MINIBATCH
-                    #print(trig)
                     log_prob_trigram = self.log_prob(trig[0],trig[1],trig[2])
-                    # print(log_prob_trigram)
-                    regularization =  (C/N)*(torch.sum(torch.square(self.X)) + torch.sum(torch.square(self.Y))) #ADD PARAMETERS IN HERE!!!!
+                    regularization =  (C/N)*(torch.sum(torch.square(self.X)) + torch.sum(torch.square(self.Y)) + torch.sum(torch.square(self.XYZ))) #ADD PARAMETERS IN HERE!!!!
                     F_i = log_prob_trigram - regularization
                     F += F_i
-                    (-F).backward()
-                    optimizer.step()
-                    optimizer.zero_grad()
-                print("loss: ", F)
+                (-F).backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                epoch_loss += F
                 batch = []
-                #print("loss at epoch " + str(i) + ": ", F/len(batch))
+            print("loss at epoch " + str(i) + ": ", epoch_loss/N)
 
     
     
