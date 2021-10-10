15537af76252f39081d07d768a64a3f7968923a7
dshenker <59290920+dshenker@users.noreply.github.com>
2021-10-10 18:28:36 -0400

    Update textcat.py

--- textcat.py
+++ textcat.py
@@ -7,7 +7,7 @@ import argparse
 import logging
 import math
 from pathlib import Path
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from probs import LanguageModel, num_tokens, read_trigrams
 import numpy as np
 log = logging.getLogger(Path(__file__).stem)  # Basically the only okay global variable.
@@ -58,8 +58,10 @@ def file_log_prob(file: Path, lm: LanguageModel) -> float:
     """
     log_prob = 0.0
     for (x, y, z) in read_trigrams(file, lm.vocab):
-        prob = lm.prob(x, y, z)  # p(z | xy)
-        log_prob += math.log(prob) 
+        #prob = lm.prob(x, y, z)  # p(z | xy)
+        #log_prob += math.log(prob)
+        prob = lm.log_prob(x, y, z).item()  # p(z | xy)
+        log_prob += prob
     return log_prob
 
 
@@ -67,17 +69,12 @@ def main():
     args = parse_args()
     logging.basicConfig(level=args.verbose)
 
-    log.info("Testing...")
+
     lm1 = LanguageModel.load(args.model1)
     lm2 = LanguageModel.load(args.model2)
     prior = args.prior
 
-    # We use natural log for our internal computations and that's
-    # the kind of log-probability that file_log_prob returns.
-    # But we'd like to print a value in bits: so we convert
-    # log base e to log base 2 at print time, by dividing by log(2).
 
-    log.info("Per-file log-probabilities:")
     
     more_likely_gen = []
     more_likely_spam = []
@@ -89,8 +86,10 @@ def main():
         file_name_slashes = file_name.split('/')
         file_name_important = file_name_slashes[-1]
         file_name_pieces = file_name_important.split('.')
+        
         file_cat = file_name_pieces[0]
-        file_length = file_name_pieces[1]
+        
+        #file_length = file_name_pieces[1]
         
         log_prob_gen: float = file_log_prob(file, lm1)
         log_prob_spam: float = file_log_prob(file, lm2)        
@@ -98,37 +97,39 @@ def main():
         log_prob_spam += math.log((1 - prior))
         if (log_prob_gen > log_prob_spam):
             more_likely_gen.append(file)
-            print("gen" + '\t' + str(file))
+            print(str(args.model1) + '\t' + str(file))
         else:
             more_likely_spam.append(file)
-            print("spam" + '\t' + str(file))
+            print(str(args.model2) + '\t' + str(file))
 
-        all_file_lengths.append(int(file_length))
-        if (file_cat == "gen") & (log_prob_spam > log_prob_gen):
-            #file_error_list.append(int(file_length))
+        #all_file_lengths.append(int(file_length))
+        if ((file_cat == "gen") | (file_cat == "en")) & (log_prob_spam > log_prob_gen):
             all_file_labels.append(0)
-        elif (file_cat == "spam") & (log_prob_gen > log_prob_spam):
-            #file_error_list.append(int(file_length))
+        #    file_error_list.append(int(file_length))
+        elif ((file_cat == "spam") | (file_cat == "sp")) & (log_prob_gen > log_prob_spam):
+        #    file_error_list.append(int(file_length))
             all_file_labels.append(0)
         else:
             all_file_labels.append(1)
+
     num_gen = len(more_likely_gen)
     num_spam = len(more_likely_spam)
     num_tot = num_gen + num_spam
     pct_gen = num_gen / num_tot * 100
     pct_spam = num_spam / num_tot * 100
-    print(str(num_gen) + " files were more likely gen (" + str(pct_gen) + "%)")
-    print(str(num_spam) + " files were more likely spam (" + str(pct_spam) + "%)")
+    print(str(num_gen) + " files were more likely gen (" + "{:.2f}".format(pct_gen) + "%)")
+    print(str(num_spam) + " files were more likely spam (" + "{:.2f}".format(pct_spam) + "%)")
     
-    #print(len(file_error_list) / len(all_file_lengths))
+ 
     print(np.mean(all_file_labels))
-    print(np.corrcoef(all_file_lengths, all_file_labels))
-   # plt.hist(file_error_list, bins = 50, alpha = 0.5,label = "errors")
-   # plt.hist(all_file_lengths, bins = 50, alpha = 0.5, label = "number files")
-   # plt.xlabel("file length")
-   # plt.ylabel("number of errors")
-   # plt.title("Relationship Between Length of File and Error Rate")
-   # plt.show()
+    #print(np.corrcoef(all_file_lengths, all_file_labels))
+    #plt.hist(file_error_list, bins = 50, alpha = 0.75,label = "error count")
+    #plt.hist(all_file_lengths, bins = 50, alpha = 0.5, label = "number files")
+    #plt.xlabel("file length")
+    #plt.ylabel("count")
+    #plt.title("Relationship Between Length of File and Error Rate")
+    #plt.legend()
+    #plt.show()
 
     #plt.hist(all_file_lengths)
     #plt.xlabel("file length")
